1dc39c0b6dd0b91a54c23932d5d8a2977bf88f2e
nuneswar <nuneswar2000@hotmail.com>
2016-03-22 14:28:34 -0300

    Comitando alterações

--- src/prodam/teste/browser/anuncio.py
+++ src/prodam/teste/browser/anuncio.py
@@ -1,12 +1,13 @@
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from Products.CMFCore.utils import getToolByName
 
 class Anuncios(BrowserView):
 
     index = ViewPageTemplateFile("overrides/anuncio.pt")
 
     def getAnuncios(self):
-        catalog = getToolByName(self, 'portal_catalog')
+        catalog = getToolByName(self.context, 'portal_catalog')
         results = catalog(portal_type='anuncio')
         print(results)
         return results
