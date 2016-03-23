3355d1bef2bf1e0a8ceda93d40a626e9b05a9b50
nuneswar <nuneswar2000@hotmail.com>
2016-03-23 10:47:49 -0300

    Comitando alterações

--- src/prodam/teste/browser/anuncio.py
+++ src/prodam/teste/browser/anuncio.py
@@ -9,15 +9,12 @@ class Anuncios(BrowserView):
     def getAnuncios(self):
         catalog = getToolByName(self.context, 'portal_catalog')
         results = catalog(portal_type='anuncio')
-        print(results)
         return results
 
     def render(self):
         return self.index()
 
 
-    def getImage(self,item):
-        return  self.context.absolute_url_path()
         
     def __call__(self):
         return self.render()
