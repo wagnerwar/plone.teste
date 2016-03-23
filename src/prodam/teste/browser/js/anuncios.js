98263b1722ac40fa7a359f16900c558046f9310b
nuneswar <nuneswar2000@hotmail.com>
2016-03-23 17:26:57 -0300

    Comitando alterações

--- src/prodam/teste/browser/js/anuncios.js
+++ src/prodam/teste/browser/js/anuncios.js
@@ -1,3 +1,32 @@
-$(document).ready(function(e){
-	alert("SCRIPT CARREGADO");
+(function($) {
+
+$(document).ready(function(){
+	$(window).load(function(){
+            var tempo = 5000;
+            var index = 1;
+       	    function exibeAnuncios(){
+              $('div.anuncio-inativo').each(function(i,v){
+                ind=i+1;
+                console.log("INDICE DE ENTRADA: " + ind);
+
+                if(ind == index){
+                    console.log("Indice sendo considerado: " + ind);
+                   $(".anuncio-ativo").removeClass("anuncio-ativo");
+                   $(this).addClass("anuncio-ativo");
+		}
+              });
+	    } 
+            var timer = setInterval(function(){
+              console.log("INDEQS: " + index);
+              exibeAnuncios();
+              //tempo = tempo*index;
+              index++;
+              if($("div.anuncio-inativo").length < index){
+                 index = 1;
+              }
+            }, tempo);
+	});
 });
+
+})(jQuery);
+
